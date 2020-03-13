2f3d7454a147da50ac6536d526ed61cc10fbbec0
Lauren Hayward <2044191@sun.ac.za>
2020-03-13 20:42:24 +0200

    Writing the main function and adding all needed methods

--- graph.py
+++ graph.py
@@ -15,10 +15,6 @@ time = ''
 
 graph = ''
 
-#Possible Ties
-restaurant1 = ''
-restaurant2 = ''
-
 #Only to be run once in it's lifetime
 def init_graph():
 	uri = "bolt://neo4j:payR900chump@localhost:8000"
@@ -78,12 +74,21 @@ def fetch(city, cuisine, day, time):
 	#All data must be in CamelCase
 	time = time.split('-')
 	day = day.lower()
-	cypher = "MATCH (rest:Business)-[:IN_CATEGORY]->(Category {id: '{cuisine}'}) WHERE rest.city='{ct}' AND rest.{d}Start>'{time_start}' AND rest.{day}End<'{time_end}'".format(ct=city, d=day, time_start=time[0], time_end=time[1])
-	graph.run(cypher).data()		#RETURNS dict object	
+	cypher = "MATCH (rest:Business)-[:IN_CATEGORY]->(Category {id: '{cuisine}'}) WHERE rest.city='{ct}' AND rest.{d}Start>'{time_start}' AND rest.{day}End<'{time_end}' RETURN rest".format(ct=city, d=day, time_start=time[0], time_end=time[1])
+	return graph.run(cypher).data()		#RETURNS list of dictionaries
+
+def get_reviews(restaurant): #dict object
+	id = restaurant.get('id')
+	cypher = "MATCH (:Business {id : '{rest}'})<-[r:REVIEWS]-(:User) RETURN r".format(rest=id)
+	return graph.run(cypher).data()
+
+def get_user_from_review(text): #property eg id, text... =  property_val
+	cypher = "MATCH (u:User)-[r:Reviews]->(:Business) WHERE r.text='{txt}' RETURN u".format(txt=text)
+	return graph.run(cypher).data()[0] #This should work...
 
 #sorts and finds which restaurant to recommend
-def recommend_rest():
-	find_most_stars()
+def recommend_rest(restaurants): #restaurants is list of dictionaries
+	#find_most_stars
 
 	if restaurant1 != restaurant2:	#tie
 		restaurant = find_most_reviews():
@@ -92,9 +97,21 @@ def recommend_rest():
 
 	return restaurant
 
-def display_stats(restaurant):
+def get_top_review(reviews): #list of dictionaries
+	#sort...
+	return review #dict
 
-def display_useful_review(restaurant):
+#information all within the dict object
+def display_stats(restaurant):
+	#name
+	#full address
+	#stars
+	#review count
+
+def display_useful_review(review, user): #review and user are dicts
+	#full text
+	#name of user
+	#stars
 
 def display_photos(restaurant):
 
@@ -103,18 +120,32 @@ def get_50_reviewers():
 def recommend_based():
 
 def main():
+	open_graph()
+
 	#Part 1: Recommend a restaurant with it's review
 	get_input()
 
-	fetch(city, cuisine, day, time)
+	#TEST CODE#
+	city = ''
+	cuisine = ''
+	day = ''
+	time = ''
+	##
 
-	restaurant = recommend_rest()
+	rest_results = fetch(city, cuisine, day, time) #rest_results is a list of dicts
+	restaurant = recommend_rest(rest_results) #restaurant should be a dict object
+	
+	reviews_result = get_reviews(restaurant) #list of dictionaries
+	top_review = get_top_review(reviews_result) #dict object
+	top_review_user = get_user_from_review(top_review.get('text'))
 	
 	display_stats(restaurant)
-	display_useful_review(restaurant)
+	display_useful_review(top_review, top_review_user)
 	display_photos(restaurant)
 
-	#Part 2: Recommend 5 more restaurants based on 50 other users
+	# Part 2: Recommend 5 more restaurants based on 50 other users
+	circle = get_social_circle(top_review_user) #circle is list of dicts
+
 	get_50_reviewers()
 	recommend_based()
 
