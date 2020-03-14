b088fbf22ff65eee0bf38006e5c6cc7e76cba6b8
Lauren Hayward <2044191@sun.ac.za>
2020-03-14 14:04:34 +0200

    adding Adam's code

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
-#import jinja
-#import flask
+from jinja2 import Template
+from flask import Flask
 from py2neo import Graph
 
 #--------------------------------------------------------------------------------#
@@ -77,47 +77,48 @@ def fetch(city, cuisine, day, time):
 	#All data must be in CamelCase
 	time = time.split('-')
 	day = day.lower()
-	cypher = "MATCH (rest:Business)-[:IN_CATEGORY]->(Category {id: '{cuisine}'}) WHERE rest.city='{ct}' AND rest.{d}Start>'{time_start}' AND rest.{day}End<'{time_end}' RETURN rest".format(ct=city, d=day, time_start=time[0], time_end=time[1])
+	cypher = "MATCH (rest:Business)-[:IN_CATEGORY]->(Category {id: '%s'}) WHERE rest.city='%s' AND rest.%sStart>'%s' AND rest.%sEnd<'%s' RETURN rest"%(city, day, time[0], time[1])
 	return graph.run(cypher).data()		#RETURNS list of dictionaries
 
 def get_reviews(restaurant): #dict object
 	id = restaurant.get('id')
-	cypher = "MATCH (:Business {id : '{rest}'})<-[r:REVIEWS]-(:User) RETURN r".format(rest=id)
+	cypher = "MATCH (:Business {id : '%s'})<-[r:REVIEWS]-(:User) RETURN r"%(id)
 	return graph.run(cypher).data()
 
 def get_user_from_review(text): #property eg id, text... =  property_val
-	cypher = "MATCH (u:User)-[r:Reviews]->(:Business) WHERE r.text='{txt}' RETURN u".format(txt=text)
+	cypher = "MATCH (u:User)-[r:Reviews]->(:Business) WHERE r.text='%s' RETURN u"%(text)
 	return graph.run(cypher).data()[0] #This should work...
 
 def get_social_circle(user):
-	cypher = "MATCH (u:User)-[:FRIENDS*1...2]->(b:User), (b:User)-[:FRIENDS]->(c:User) RETURN b, c"
+	id = user.get('id')
+	cypher = "MATCH (u:User {id : '%s'})-[:FRIENDS*1...2]->(b:User), (b:User)-[:FRIENDS]->(c:User) RETURN b, c"%(id)
 #sorts and finds which restaurant to recommend
 
 def get_reviews_by_50(users, city, cuisine): #users are list of dict, other are strings
+		full_list = list()
+		for user in users:
+			id = user.get('id')
+			cypher = "MATCH (:User {id : '%s')-[r:REVIEWS]->(:Business) RETURN r"%(id)
+			temp_list = graph.run(cypher).data()
+			full_list = full_list + temp_list
 
+		return full_list
 
 #--------------------------------------------------------------------------------#
 #	ADAM & JOHAN
 #--------------------------------------------------------------------------------#
 
+#Sort by stars, tie break by review count
 def recommend_rest(restaurants): #restaurants is list of dictionaries
-	#find_most_stars
-
-	if restaurant1 != restaurant2:	#tie
-		restaurant = find_most_reviews():
-	else:
-		restaurant = restaurant1
-
-	return restaurant
+	sorted_list  = sorted(restaurants, reverse= True, key= lambda k: (k['stars'],k['review_count']))
+    return sorted_list[0]
 
 def get_top_review(reviews): #list of dictionaries
-	#sort...
-	return review #dict
+	sorted_list = sorted(reviews, reverse= True, key= lambda k: (k['useful']) )
+    return sorted_list[0]
 
 #sort by highest review count
 def get_50_reviewers(users):
-
-def recommend_based():
 
 def filter_reviews(reviews):
 
@@ -130,10 +131,10 @@ def get_input():
 
 #information all within the dict object
 def display_stats(restaurant):
-	#name
+	name = restaurant.get('name')
 	#full address
-	#stars
-	#review count
+	stars = restaurant.get('stars')
+	#review_count = restaurant.get('review_count')
 
 def display_useful_review(review, user): #review and user are dicts
 	#full text
@@ -166,16 +167,18 @@ def main():
 	top_review = get_top_review(reviews_result) #dict object
 	top_review_user = get_user_from_review(top_review.get('text'))
 	
-	display_stats(restaurant)
-	display_useful_review(top_review, top_review_user)
-	display_photos(restaurant)
-
 	# Part 2: Recommend 5 more restaurants based on 50 other users
 	circle = get_social_circle(top_review_user) #circle is list of dicts
 
 	top_50  = get_50_reviewers(circle) #top_50 list of dict
 	reviews_by_50 = get_reviews_by_50(top_50, city, cuisine) #list of dict
 	top_5_restaurants = filter_reviews(reviews_by_50) #list of dict
+	
+	#Part 3: Display information
+
+	display_stats(restaurant)
+	display_useful_review(top_review, top_review_user)
+	display_photos(restaurant)
 
 	for restaurant in top_5_restaurants:
 		display_stats(restaurant)
